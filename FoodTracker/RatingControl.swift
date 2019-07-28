c2285dfa253acf9834aeb1528580c5cbb22c9b0d
Joel Yek <joevo2@gmail.com>
2019-07-28 18:31:02 +0800

    wip

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,19 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Joel Yek on 28/04/2019.
+//  Copyright © 2019 Joel Yek. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        <#code#>
+    }
+
+}
